631434fbe2bbab1171ab02920a8232930fd29782
clRcoDE <45456252+clRcoDE@users.noreply.github.com>
2019-02-13 10:00:43 +0330

    minor changes

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View , StyleSheet,TouchableHighlight} from 'react-native'
+import { Text, View , StyleSheet,TouchableHighlight,ScrollView} from 'react-native'
 import {connect} from 'react-redux'
 import {getUsers,loadData} from '../Services/Actions/Action'
 class Main extends Component {
@@ -10,16 +10,38 @@ this.props.dispatch(loadData())
     }
   render() {
     return (
-      <View>
-<TouchableHighlight onPress={this.changeStore.bind(this)}><Text>Press</Text></TouchableHighlight>
+      <View style={styles.container}>
+<TouchableHighlight style={styles.buttonWrapper} onPress={this.changeStore.bind(this)}><Text>Press To Fetch With Thunk</Text></TouchableHighlight>
 
         {/* <Text>{this.props.names}</Text> */}
+        <ScrollView >
+
+        <View style={styles.lister}>
         {this.props.names.map((item,index)=><Text  key={index}>{item}</Text>)}
+
+        
+        </View>
+        </ScrollView>
       </View>
     )
   }
 }
+const styles=StyleSheet.create({
+  container:{
+    flex:1
+  },
+  buttonWrapper:{
+    height:200,
+    backgroundColor:'#666',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lister:{
+    alignItems:'center',
+    padding: 25,
+  }
 
+})
 
 const mapStateToProps = state => {
     return {
